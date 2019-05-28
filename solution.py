2da96e19cbb4273955cfd5d339e0d3d225c78ab9
Gideon Paul <gidzpaul95@gmail.com>
2019-05-28 18:39:33 -0400

    Coded a method to return a list of routes given the service_name.

--- solution.py
+++ solution.py
@@ -1,3 +1,10 @@
 class Services:
     #The standard set of services
     available_services = {'strava': ["SRT", "CVT", "Perkiomen"], 'rwgps' :["CVT", "Perkiomen", "Welsh Mountain"], 'komoot' : ["SRT", "Welsh Mountain", "Oaks to Philly"]}
+
+    def get_service(self, service_name):
+	'''
+	Given a service name as a string, returns the routes associated with this service.
+	'''
+        return self.available_services[service_name]
+
