745c3f87333c8e70d2a815b465f186962e0389a3
Gideon Paul <gidzpaul95@gmail.com>
2019-05-28 18:52:48 -0400

    Coded and tested all routes method. Also, corrected some indentation errors.

--- solution.py
+++ solution.py
@@ -3,15 +3,15 @@ class Services:
     available_services = {'strava': ["SRT", "CVT", "Perkiomen"], 'rwgps' :["CVT", "Perkiomen", "Welsh Mountain"], 'komoot' : ["SRT", "Welsh Mountain", "Oaks to Philly"]}
 
     def get_service(self, service_name):
-	'''
-    Given a service name as a string, returns the routes associated with this service.
-    '''
+        '''
+        Given a service name as a string, returns the routes associated with this service.
+        '''
         return self.available_services[service_name]
 
     def get_all_service_names(self):
-    '''
-    A helper function to just return the names of all the available services.
-    '''
+        '''
+        A helper function to just return the names of all the available services.
+        '''
         return self.available_services.keys()
 
 class Helper:
@@ -22,13 +22,26 @@ class Helper:
     services = Services()
     service_names = services.get_all_service_names()
 
+    def get_all_routes(self):
+        '''
+        Return all the possible routes as a list.
+        '''
+        all_routes = []
+
+        #Iterate over each service
+        for service in self.service_names:
+            #Add each element to routes list
+            all_routes.extend(self.services.get_service(service))
+
+        return all_routes
+
 
 def main():
     #Create an object of helper class. We use this object to print the required output.
     helper = Helper()
 
     #Output
-    print("All routes:")
+    print("All routes:", helper.get_all_routes())
     print("Unique routes:")
     print("For user 42:")
     print("For user 42 services [\"komoot\",\"rwgps\"]:")
