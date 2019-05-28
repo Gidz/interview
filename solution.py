32a209606214d08889ea5bd52c4533af8ac4519a
Gideon Paul <gidzpaul95@gmail.com>
2019-05-28 18:40:42 -0400

    A helper method to return the names of available services as list.

--- solution.py
+++ solution.py
@@ -2,9 +2,15 @@ class Services:
     #The standard set of services
     available_services = {'strava': ["SRT", "CVT", "Perkiomen"], 'rwgps' :["CVT", "Perkiomen", "Welsh Mountain"], 'komoot' : ["SRT", "Welsh Mountain", "Oaks to Philly"]}
 
+
     def get_service(self, service_name):
 	'''
 	Given a service name as a string, returns the routes associated with this service.
 	'''
         return self.available_services[service_name]
 
+    def get_all_service_names(self):
+	'''
+	A helper function to just return the names of all the available services.
+	'''
+        return self.available_services.keys()
