4137244d1580536ef3182544d500b05227bc814d
Gideon Paul <gidzpaul95@gmail.com>
2019-05-28 18:48:07 -0400

    Coded the Helper class skeleton.

--- solution.py
+++ solution.py
@@ -14,14 +14,27 @@ class Services:
     '''
         return self.available_services.keys()
 
+class Helper:
+    '''
+    A helper class which can host all the business logic a.k.a required methods to get the output.
+    '''
+    #Create an object of Services class and get all the service name which might come in handy later
+    services = Services()
+    service_names = services.get_all_service_names()
 
 
 def main():
+    #Create an object of helper class. We use this object to print the required output.
+    helper = Helper()
+
+    #Output
     print("All routes:")
     print("Unique routes:")
     print("For user 42:")
     print("For user 42 services [\"komoot\",\"rwgps\"]:")
     
 
+#If this program is being run from command line directly, execute the main method.
+#Also, the entry point of the program.
 if __name__=="__main__":
     main()
