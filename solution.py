f0129ffd692e73d1715ecd92cad4f241f3eaa5a9
Gideon Paul <gidzpaul95@gmail.com>
2019-05-28 18:38:38 -0400

    Added standard services.

---
+++ solution.py
@@ -0,0 +1,3 @@
+class Services:
+    #The standard set of services
+    available_services = {'strava': ["SRT", "CVT", "Perkiomen"], 'rwgps' :["CVT", "Perkiomen", "Welsh Mountain"], 'komoot' : ["SRT", "Welsh Mountain", "Oaks to Philly"]}
